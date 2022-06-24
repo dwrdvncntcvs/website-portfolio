7f5cd02c457bc6d1cd1ee224a6a3a33e8f6ba114
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-06-24 14:05:45 +0800

    Basic sign in form created without design

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,6 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./signIn.scss";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signInAction = (e) => {
+    e.preventDefault();
+    const credentials = {
+      email,
+      password,
+    };
+
+    console.log(credentials)
+  };
+
+  const inputFields = [
+    {
+      type: "email",
+      placeholder: "Email Address",
+      value: email,
+      action: (e) => setEmail(e.target.value),
+    },
+    {
+      type: "password",
+      placeholder: "Password",
+      value: password,
+      action: (e) => setPassword(e.target.value),
+    },
+  ];
+
+  return (
+    <div>
+      <form onSubmit={signInAction}>
+        {inputFields.map(({ type, placeholder, value, action }, i) => (
+          <input
+            key={i}
+            type={type}
+            placeholder={placeholder}
+            value={value}
+            onChange={action}
+          />
+        ))}
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
 }

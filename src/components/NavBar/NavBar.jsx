72bf715c313361e104f251351ec3773bdc25fac9
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-06-24 22:10:47 +0800

    Minor changes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./navBar.scss";
+import { useLocation } from "react-router-dom";
 
 export default function NavBar() {
+  const location = useLocation();
+
+  if (location.pathname === "/sign-in") return null;
+
   return <div>NavBar</div>;
 }

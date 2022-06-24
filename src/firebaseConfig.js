09934ba139b10058ac53ab971a6bb796b5e051a6
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-06-24 13:50:46 +0800

    Firebase config, deleted some files, and dependeciess installed

---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmlFW4MyfWPM-7p4E8Z_fxK_oLGytv6PA",
+  authDomain: "dwrdvncntcvs-portfolio.firebaseapp.com",
+  projectId: "dwrdvncntcvs-portfolio",
+  storageBucket: "dwrdvncntcvs-portfolio.appspot.com",
+  messagingSenderId: "159180020678",
+  appId: "1:159180020678:web:e047ec057559bc639d30c7",
+};
+
+const app = initializeApp(firebaseConfig);

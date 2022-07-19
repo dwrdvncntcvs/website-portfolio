55dbd98ce311e95e9db1e3401fd8722753fdbc3b
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-07-19 19:15:47 +0800

    Preview images image navigation

--- src/components/PreviewImages/PreviewImages.jsx
+++ src/components/PreviewImages/PreviewImages.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { HiX } from "react-icons/hi";
+import React, { useState } from "react";
+import { HiX, HiChevronLeft, HiChevronRight } from "react-icons/hi";
 import "./previewImages.scss";
 
 export default function PreviewImages({
@@ -9,6 +9,7 @@ export default function PreviewImages({
   imageId,
 }) {
   const [preview, setPreview] = previewState;
+  const [imgId, setImgId] = useState(0);
 
   const rearrangeImage = () => {
     const imagesArr = images.filter((image) => image !== mainImage);
@@ -22,16 +23,46 @@ export default function PreviewImages({
     setPreview(!preview);
   };
 
+  const viewImage = (direction) => {
+    if (direction === "right") {
+      if (imgId === newImages.length - 1) return;
+
+      return setImgId(imgId + 1);
+    }
+
+    if (direction === "left") {
+      if (imgId === newImages.length - newImages.length) return;
+
+      return setImgId(imgId - 1);
+    }
+  };
+
   return (
     <div className="pi__main-container">
       <div className="pi__image-prev">
         <button onClick={closeAction}>
           <HiX />
         </button>
-        <img src={mainImage} alt="" />
+        <button
+          id="pi__left"
+          className={
+            imgId === newImages.length - newImages.length
+              ? "pi__btn-disable"
+              : ""
+          }
+          onClick={() => viewImage("left")}
+        >
+          <HiChevronLeft />
+        </button>
+        <img src={newImages[imgId]} alt="" />
+        <button
+          id="pi__right"
+          className={imgId === newImages.length - 1 ? "pi__btn-disable" : ""}
+          onClick={() => viewImage("right")}
+        >
+          <HiChevronRight />
+        </button>
       </div>
-      {/* <button id="pi__left"></button>
-      <button id="pi__right"></button> */}
     </div>
   );
 }

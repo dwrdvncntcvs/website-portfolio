8f1e4bd7c14b04ad2cbfc035b2ede60d07bc3426
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-07-20 10:23:31 +0800

    Sample structure for certificate outlet

--- src/pages/Certificates/Certificates.jsx
+++ src/pages/Certificates/Certificates.jsx
@@ -3,9 +3,16 @@ import { OutletHeader } from "../../components";
 import "./certificates.scss";
 
 export default function Certificates() {
+  const array = [1, 2, 3, 4, 5, 6, 7, 8 ];
+
   return (
     <div className="ct__main-container">
       <OutletHeader title={"certificates"} />
+      <section className="ct__content-container">
+        {array.map(() => (
+          <div className="ct__card"></div>
+        ))}
+      </section>
     </div>
   );
 }

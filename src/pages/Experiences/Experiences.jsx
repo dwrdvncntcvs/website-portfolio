7d65cb7fe0633b1cb38bc246672083d506fc1734
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2022-07-23 10:23:20 +0800

    Basic structure of experiences outlet

--- src/pages/Experiences/Experiences.jsx
+++ src/pages/Experiences/Experiences.jsx
@@ -6,6 +6,21 @@ export default function Experiences() {
   return (
     <div className="exp__main-container">
       <OutletHeader title={"experiences"} />
+      <section className="exp__content-container">
+        <div className="exp__card-container">
+          <section className="exp__company-container">
+            <p>Company logo img</p>
+            <h1>Role</h1>
+            <h2>Company Name</h2>
+            <h3>Job Duration</h3>
+          </section>
+          <section>
+            <h1>Work</h1>
+            <h2>title</h2>
+            <p>Description</p>
+          </section>
+        </div>
+      </section>
     </div>
   );
 }
